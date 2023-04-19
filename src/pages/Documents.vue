a4b5d41044a9b780198d7ad48ead4bede5fa143d
Nikita Antonov <nikita45454@gmail.com>
2023-04-19 17:59:40 +0500

    fix: стилевые правки

--- src/pages/Documents.vue
+++ src/pages/Documents.vue
@@ -2,7 +2,7 @@
   <app-page class="2xl:tw-pt-[100px] tw-relative">
     <div class="wrapper decor-rock">
       <div
-        class="tw-flex md:tw-justify-end xl:tw-justify-normal xl:tw-pr-[25%] 2xl:tw-pr-[18%] tw-mt-30">
+        class="tw-flex md:tw-justify-end lg:tw-justify-normal lg:tw-pr-[25%] 2xl:tw-pr-[18%] tw-mt-30">
         <div class="">
           <h1 class="h1 tw-mb-20">Документы</h1>
           <div class="tw-text-base tw-text-gray tw-leading-120 2xl:tw-text-md">
