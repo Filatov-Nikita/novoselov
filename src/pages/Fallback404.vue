bafd6de9e84aa9eb30bee1297dfe78eb6d0e90c2
Nikita Antonov <nikita45454@gmail.com>
2023-04-19 12:45:24 +0500

    feat: стилизует 404 страницу

--- src/pages/Fallback404.vue
+++ src/pages/Fallback404.vue
@@ -1,9 +1,17 @@
 <template>
   <app-page class="2xl:tw-pt-[100px] page-pb">
-    <div class="wrapper tw-relative decor-rock after:tw-content-none 2xl:after:tw-content-['']">
-      <div class="2xl:tw-flex 2xl:tw-justify-between">
-        <div class="tw-mb-20">
-          <p class="tw-font-extrabold tw-text-lg tw-leading-100 md:tw-text-xl 2xl:tw-text-xl10">упс ...</p>
+    <div
+      class="wrapper tw-relative after:tw-content-none 2xl:after:tw-content-['']">
+      <div class="xl:tw-flex xl:tw-justify-between">
+        <div class="">
+          <p
+            class="tw-font-stolz -tw-tracking-sm tw-text-lg tw-leading-120 md:tw-text-xl 2xl:tw-text-xl10 tw-mb-20 md:tw-mb-40 xl:tw-mb-20">
+            Упс ...
+          </p>
+          <h1
+            class="tw-text-gray tw-text-base tw-leading-120 tw-max-w-[260px] tw-mb-60 md:tw-max-w-[600px] xl:tw-text-md md:tw-font-extrabold md:tw-mb-60">
+            Извините, страница не найдена
+          </h1>
         </div>
         <div class="2xl:tw-basis-[862px]">
           <h1 class="tw-text-md tw-leading-120 tw-text-secondary tw-max-w-[260px] tw-mb-20 md:tw-max-w-[600px] md:tw-text-lg md:tw-font-extrabold md:tw-mb-60">
