fb0e9cd4015125fbb583f326f0c1db68310d0a43
Ralfik <developeraul@gmail.com>
2023-04-19 16:33:17 +0500

    страница документы

--- src/pages/Documents.vue
+++ src/pages/Documents.vue
@@ -1,13 +1,20 @@
 <template>
-  <app-page class="2xl:tw-pt-[100px]">
+  <app-page class="2xl:tw-pt-[100px] tw-relative">
     <div class="wrapper decor-rock">
-      <div class="2xl:tw-flex 2xl:tw-justify-between">
-        <h1 class="h1 tw-mb-20">Документы</h1>
-        <div class="tw-text-md tw-text-secondary tw-leading-120 tw-max-w-[248px] md:tw-max-w-[600px] md:tw-font-extrabold md:tw-text-lg 2xl:tw-text-xl 2xl:tw-max-w-[862px] 2xl:tw-leading-100 2xl:tw-pt-30">
-          Разрешительная документация на&nbsp;сайте
-          <a class="tw-text-[#3AB2ED] tw-underline md:tw-no-underline" :href="$store.state.domrf" target="_blank">
-            наш.дом.рф
-          </a>
+      <div
+        class="tw-flex md:tw-justify-end xl:tw-justify-normal xl:tw-ml-[55%] 2xl:tw-ml-[51%] tw-mt-30">
+        <div class="">
+          <h1 class="h1 tw-mb-20">Документы</h1>
+          <div class="tw-text-base tw-text-gray tw-leading-120 2xl:tw-text-md">
+            Разрешительная документация <br />
+            на сайте
+            <a
+              class="tw-text-blue tw-underline"
+              :href="$store.state.domrf"
+              target="_blank">
+              наш.дом.рф
+            </a>
+          </div>
         </div>
       </div>
     </div>
