c950c5001cccd5d74a0ad0520bafc78931ad654c
Nikita Antonov <nikita45454@gmail.com>
2023-05-05 18:31:31 +0500

    feat: адаптивка для выбора на фасаде

---
+++ src/components/PopupStorey.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="storey-popup">
+    <div>
+      <p class="tw-text-xl tw-text-secondary">
+        {{ storey.entrance }}
+      </p>
+      <p>секция</p>
+    </div>
+    <div>
+      <p class="tw-text-xl tw-text-secondary">
+        {{ storey.number }}
+      </p>
+      <p>этаж</p>
+    </div>
+    <div>
+      <p class="tw-text-xl tw-text-secondary">
+        {{ storey.flats_counters.free }}
+      </p>
+      <p>
+        квартир <br />
+        свободно
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      storey: {
+        required: true,
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.storey-popup {
+  z-index: 10;
+  padding: 16px;
+  border-radius: 16px;
+  background: #f5f5f5;
+  backdrop-filter: blur(7px);
+  color: theme("colors.gray");
+  font-size: 14px;
+  position: fixed;
+  left: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+@screen lg {
+  .storey-popup {
+    left: 25px;
+  }
+}
+</style>
---
+++ src/components/PopupTouchMove.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="touch-move" @touchstart="hide">
+    <div class="tw-basis-[215px] tw-text-white tw-text-sm tw-flex tw-items-center">
+      <AppIcon class="tw-mr-24" name="swipe" size="48px" fill="white" />
+      <p>Перемещайтесь по экрану для просмотра всего литера</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['hide'],
+  methods: {
+    hide() {
+      this.$emit('hide');
+    }
+  }
+}
+</script>
+<style scoped>
+  .touch-move {
+    background: rgb(138, 152, 167, 0.8);
+    position: absolute;
+    inset: 0;
+    z-index: 200;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
